336867cbeeb0c9688b12bcf17a67b84c9e91226a
Jorge Paul <jserranopaul@gmail.com>
2021-04-12 15:08:32 +0200

    Context API

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,9 @@
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
 const SignUp = () => {
-  return <div>Signup</div>;
+  const { isAuthenticated, error } = useContext(AuthContext);
+  return <div>{isAuthenticated ? 'HEllo' : 'not auth'}</div>;
 };
 
 export default SignUp;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import AuthState from './context/AuthContext';
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthState>
+        <App />
+      </AuthState>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
