49712ab5a82db7e9f53c60f7ffb40d8a726a7613
Jorge Paul <jserranopaul@gmail.com>
2021-04-13 21:05:16 +0200

    get pokemons

--- src/components/Battleground.js
+++ src/components/Battleground.js
@@ -1,5 +1,62 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Battleground = () => {
-  return <div>PRIVATE AREA</div>;
+  const [team, setTeam] = useState();
+  const [enemyTeam, setEnemyTeam] = useState();
+  const [usrLvl, setUsrLvl] = useState(1);
+  useEffect(() => {
+    const types = Array(usrLvl + 2).fill('');
+    const getTeam = async () => {
+      try {
+        const typesResults = await Promise.all(
+          types.map((type, i) => axios.get(`https://pokeapi.co/api/v2/type/${i + 1}`))
+        );
+        const pokemonsResults = await Promise.all(
+          typesResults.map(({ data: { pokemon } }) =>
+            axios.get(pokemon[Math.floor(Math.random() * pokemon.length + 1)].pokemon.url)
+          )
+        );
+        const pokemonsForTeams = pokemonsResults.map(({ data }) => ({ ...data }));
+        return pokemonsForTeams;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getTeam().then(pokemons => setTeam(pokemons));
+    getTeam().then(pokemons => setEnemyTeam(pokemons));
+  }, []);
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          {team ? (
+            team.map(p => (
+              <div key={p.id}>
+                <img src={p.sprites.front_default} style={{ transform: 'scaleX(-1)' }} />
+                <div>{p.name}</div>
+              </div>
+            ))
+          ) : (
+            <div>Loading</div>
+          )}
+        </div>
+        <div className='col'>
+          {enemyTeam ? (
+            enemyTeam.map(p => (
+              <div key={p.id}>
+                <img src={p.sprites.front_default} />
+                <div>{p.name}</div>
+              </div>
+            ))
+          ) : (
+            <div>Loading</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Battleground;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,7 +9,13 @@ const Navigation = () => {
   return (
     <Navbar bg='light' expand='lg'>
       <Link to='/'>
-        <Navbar.Brand>PokeFight</Navbar.Brand>
+        <Navbar.Brand>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png'
+            alt='Pokemon'
+            width='150px'
+          />
+        </Navbar.Brand>
       </Link>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
